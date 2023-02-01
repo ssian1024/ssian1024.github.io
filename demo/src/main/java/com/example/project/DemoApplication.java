bf35ba5e84e0dfa5bf3c0a465a4fe7d65d10e1b1
KKtae <20170584@gm.hannam.ac,kr>
2023-02-02 07:00:57 +0900

    First : 회원정보 관리 (수정중)

--- demo/src/main/java/com/example/project/DemoApplication.java
+++ demo/src/main/java/com/example/project/DemoApplication.java
@@ -1,13 +1,11 @@
-package com.example.demo;
+package com.example.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication// 날짜 자동 입력 활성화
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-}
+	}}
